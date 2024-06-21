fd20a247a107efa95cb272cc31dab2130acf87e9
bashybaranaba <bashybaranaba@gmail.com>
2024-06-22 02:03:50 +0300

    feat: add game agent logic

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -11,7 +11,18 @@ export async function POST(req: Request) {
   const model = ollama.ChatTextGenerator({ model: "llama3" }).withChatPrompt();
 
   const prompt = {
-    system: "You are an AI chatbot. Follow the user's instructions carefully.",
+    system: `You are an AI game agent in a mystery game. Your goal is to challenge a player to guess a single word from a story based mystery.You have the freedom to select the story and the word to be guessed. To start the game the player will tell you to start the game. You can then start the game by providing the story and the word to be guessed.
+      Keep it short, sweet and simple. The player will have to guess the word based on the story you provide. If the player guesses the word correctly, you can congratulate them. If the player guesses the word incorrectly, you can provide a hint or ask them to try again.
+      Here is an example of a story based mystery and a word to be guessed:
+        story: "Detective Watson entered the dimly lit room, noticing an old desk in the corner.On the desk, there were scattered papers and an ink bottle nearby.He knew the culprit must have left something behind when writing the letter."
+        word: "quill"
+      Feel free to create your own story and word to be guessed. Make sure the story is short and engaging and the word is challenging to guess.
+      You can prompt the player for the word by saying something like "What could it be?" and avoid asking the player to guess the word directly.
+      Also, have a short and sweet one liner for starting the game. For example, "It's great you're here detective...(story)..."
+      No need to tell the player that they can ask for hints or clarification. They can ask you for hints or clarification if they need it. You can provide hints or clarification as needed.
+      While providing hints, you can provide a hint that is related to the story. Do not mention that it is a hint and avoid asking the player to guess the word directly.
+      Note that since this game is being played in a web based environment, I need you to provide the word to be guessed at the end of the first message but wrapped in **. For example, "What could it be? **answer**" which will ne hidden to the player but visible to me.
+    `,
 
     // map Vercel AI SDK Message to ModelFusion ChatMessage:
     messages: asChatMessages(messages),
--- components/game/EnigmaAgent.tsx
+++ components/game/EnigmaAgent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useChat } from "ai/react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -8,9 +8,23 @@ import PlayerInputBox from "./PlayerInputBox";
 export default function EnigmaAgent() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   const bottomRef = useRef<HTMLDivElement | null>(null);
+  const [correctAnswer, setCorrectAnswer] = useState<string>("");
+  const hiddenPattern = /\*\*(.*?)\*\*/;
 
   useEffect(() => {
     bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  useEffect(() => {
+    //set the correct answer to the word between the double asterisks
+    const answer = messages.find((message) => message.role !== "user");
+    if (answer) {
+      const match = answer.content.match(hiddenPattern);
+      console.log("match", match);
+      if (match) {
+        setCorrectAnswer(match[1]);
+      }
+    }
   }, [messages]);
 
   return (
@@ -29,19 +43,20 @@ export default function EnigmaAgent() {
                 </Avatar>
               )}
               <div
-                className={`w-72 m-1 p-3 rounded-lg text-sm ${
+                className={`w-72 m-1 p-3 rounded-lg text-sm  ${
                   message.role === "user"
                     ? " bg-amber-50 ml-auto"
                     : "bg-indigo-50"
                 }`}
               >
-                {message.content}
+                {message.content.replace(/\*\*[^*]+\*\*/g, "")}
               </div>
               <br />
               <br />
             </div>
           ))}
           <div ref={bottomRef} />
+          answer: {correctAnswer}
         </div>
       </ScrollArea>
       <PlayerInputBox
